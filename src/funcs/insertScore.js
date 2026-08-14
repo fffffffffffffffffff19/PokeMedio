93ee47eaf0b5a4846f9d3516d376f7af6cc9a549
@fffffffffffffffffff19 <davischiavon21@gmail.com>
2026-08-14 17:49:08 -0300

    feat: create db inserts

---
+++ src/funcs/insertScore.js
@@ -0,0 +1,13 @@
+import db from "../database/db.js"
+
+export function insertScore(user) {
+	const stmt = db.prepare(`
+		INSERT INTO user_ranks (user_id, score, updated_at)
+		VALUES (?, ?, CURRENT_TIMESTAMP)
+		ON CONFLICT(user_id) DO UPDATE SET
+			score = score + excluded.score,
+			updated_at = CURRENT_TIMESTAMP
+	`);
+
+	stmt.run(user.id, 1);
+}
