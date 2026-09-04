329ec43836344c38915ea51bd1fd3a1daee5fbe1
@fffffffffffffffffff19 <davischiavon21@gmail.com>
2026-09-04 16:53:19 -0300

    refactor: move GameLoop class to a common location

--- src/commands/play/gameMode/classicMode.js
+++ src/commands/play/gameMode/classicMode.js
@@ -2,17 +2,16 @@ import { ActionRowBuilder, ComponentType } from 'discord.js';
 import { questionEmbed, correctEmbed, wrongEmbed, timeoutEmbed, newPoint } from '../embeds.js';
 import { medicineBtn, pokemonBtn, stopBtn } from '../buttons.js';
 import { insertScore } from '../../../funcs/insertScore.js';
-import { getRandomGameItem } from '../../../funcs/getRandom.js';
-import GameLoop from '../../../class/GameLoop.js';
+import GameLoop from '../../../class/gameLoop.js';
 
 export default async (interaction) => {
     try {
+        const pokeButton = pokemonBtn();
+        const medicineButton = medicineBtn();
+        const stopButton = stopBtn();
+
         const gameLoop = new GameLoop(interaction, {
             onStart: async (item) => {
-                const pokeButton = pokemonBtn();
-                const medicineButton = medicineBtn();
-                const stopButton = stopBtn();
-
                 const row = new ActionRowBuilder().addComponents(pokeButton, medicineButton, stopButton);
 
                 const response = await interaction.reply({
@@ -36,7 +35,7 @@ export default async (interaction) => {
                 stopButton.setDisabled(true);
 
                 if (isCorrect) {
-                    await insertScore(i.user);
+                    insertScore(i.user);
 
                     await i.update({
                         embeds: [correctEmbed(i, item), newPoint(i.user)],
