51cc961725ec2619a8dcd2a0f4a65ef9b845dffe
@fffffffffffffffffff19 <davischiavon21@gmail.com>
2026-09-04 20:39:38 -0300

    refactor: update component collector options

--- src/commands/play/gameMode/classicMode.js
+++ src/commands/play/gameMode/classicMode.js
@@ -17,7 +17,7 @@ export default async (interaction) => {
                 const response = await interaction.reply({
                     embeds: [questionEmbed(item)],
                     components: [row],
-                    fetchReply: true
+                    withResponse: true
                 });
 
                 return response.createMessageComponentCollector({
